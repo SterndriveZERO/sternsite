33037fd612fa78b0d27e9c2e0025e3087236320e
stern <sterndrivezero@gmail.com>
2026-05-13 18:32:54 -0400

    render setup

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/guides")
-def guides():
-    return render_template("guides.html")
+@app.route("/pvp")
+def pvp():
+    return render_template("pvp.html")
+
+@app.route("/sword")
+def sword():
+    return render_template("sword.html")
+
+@app.route("/crystal")
+def crystal():
+    return render_template("crystal.html")
+
+@app.route("/smp")
+def smp():
+    return render_template("smp.html")
+
+@app.route("/uhc")
+def uhc():
+    return render_template("uhc.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
