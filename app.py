037eb1cf0bcc42c6f6051aec4fcc5b09139ecff7
stern <sterndrivezero@gmail.com>
2026-05-13 21:38:54 -0400

    render setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,25 +6,84 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route("/pvp")
 def pvp():
     return render_template("pvp.html")
+
+
+@app.route("/pvp/beginner")
+def beginner():
+    return redirect(url_for("beginner_settings"))
+
+
+@app.route("/pvp/beginner/settings")
+def beginner_settings():
+    return render_template("pvp/beginner/settings.html")
+
+
+@app.route("/pvp/beginner/mods")
+def beginner_mods():
+    return render_template("pvp/beginner/mods.html")
+
+
+@app.route("/pvp/beginner/texture-packs")
+def beginner_texture_packs():
+    return render_template("pvp/beginner/texture-packs.html")
+
+
+@app.route("/pvp/beginner/tips")
+def beginner_tips():
+    return render_template("pvp/beginner/tips.html")
+
 
 @app.route("/sword")
 def sword():
     return render_template("sword.html")
 
-@app.route("/crystal")
-def crystal():
-    return render_template("crystal.html")
+@app.route("/sword/basic")
+def basic():
+    return render_template("sword/basic.html")
 
-@app.route("/smp")
-def smp():
-    return render_template("smp.html")
+@app.route("/sword/intro")
+def intro():
+    return render_template("sword/intro.html")
 
-@app.route("/uhc")
-def uhc():
-    return render_template("uhc.html")
+@app.route("/sword/core")
+def core():
+    return render_template("sword/core.html")
 
+@app.route("/sword/intermediate")
+def intermediate():
+    return render_template("sword/intermediate.html")
+
+@app.route("/sword/combo")
+def combo():
+    return render_template("sword/combo.html")
+
+@app.route("/sword/defense")
+def defense():
+    return render_template("sword/defense.html")
+
+@app.route("/sword/advanced")
+def advanced():
+    return render_template("sword/advanced.html")
+
+@app.route("/sword/counters")
+def counters():
+    return render_template("sword/counters.html")
+
+@app.route("/sword/playstyle")
+def playstyle():
+    return render_template("sword/playstyle.html")
+
+@app.route("/sword/sweeps")
+def sweeps():
+    return render_template("sword/sweeps.html")
+
+@app.route("/sword/skills")
+def skills():
+    return render_template("sword/skills.html")
+    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
